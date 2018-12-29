558d18dc175e296d5b5eae090ef5a8be0969b95e
Allard charles <charlesal@hotmail.fr>
2018-12-30 00:10:57 +0100

    branch

--- src/main.java
+++ src/main.java
@@ -4,7 +4,7 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World 2");
-		System.out.println("My first commit");
+		System.out.println("My first commit on branch");
 	}
 
 }
